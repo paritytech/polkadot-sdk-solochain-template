1b8a610d5716fbe2e5f8e0ebaf151eaa92d418d8
prabesh130 <adhikariprabesh828@gmail.com>
2026-03-16 23:52:30 +0545

    create the two node for the validation

--- runtime/src/configs/mod.rs
+++ runtime/src/configs/mod.rs
@@ -24,6 +24,7 @@
 // For more information, please refer to <http://unlicense.org>
 
 // Substrate and Polkadot dependencies
+use frame_system::EnsureRoot;
 use frame_support::{
 	derive_impl, parameter_types,
 	traits::{ConstBool, ConstU128, ConstU32, ConstU64, ConstU8, VariantCountOf},
@@ -163,12 +164,16 @@ impl pallet_template::Config for Runtime {
 	type WeightInfo = pallet_template::weights::SubstrateWeight<Runtime>;
 }
 parameter_types! {
-    pub const MaxCandidates: u64 = 100;
+    pub const MaxCandidates: u32= 20;
     pub const MaxNameLength: u64 = 256;
+	pub const MaxCandidatesNameLen: u32= 64;
 }
 
 impl pallet_voting::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
     type MaxEncryptedVoteSize = ConstU32<2048>;
     type MaxBlindSignatureSize = ConstU32<512>;
+	type MaxCandidates=MaxCandidates;
+	type AdminOrigin=EnsureRoot<AccountId>;
+	type MaxCandidateNameLen=ConstU32<20>;
 }
